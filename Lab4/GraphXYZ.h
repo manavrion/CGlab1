ecb61e6b64835f2c3490d521c9968ca5eface516
manavrion <manavrion@yandex.ru>
2017-05-28 15:09:52 +0500

    refactored GraphXYZ

--- Lab4/GraphXYZ.h
+++ Lab4/GraphXYZ.h
@@ -24,7 +24,6 @@ struct GraphXYZ : public OldGraphXYZ::GraphXYZ {
 		graphics.DrawString(L"Y", -1, &font, PointF(center + 10, center / 4), &textBrush);
 		graphics.DrawString(L"X", -1, &font, PointF(center * 1.75, center + 9), &textBrush);
 		graphics.DrawString(L"Z", -1, &font, PointF(center/2, center * 2 - center / 2 + 9), &textBrush);
-
 	};
 
 
@@ -42,8 +41,6 @@ struct GraphXYZ : public OldGraphXYZ::GraphXYZ {
 
 		graphics.DrawString(L"Y", -1, &font, PointF(center.X + 10, 20), &textBrush);
 		graphics.DrawString(L"X", -1, &font, PointF(center.X * 1.85, center.X + 9), &textBrush);
-
-
 	};
 };
 
